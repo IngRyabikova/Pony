baaf36d14d08b5d346c40aac0c63b6e21da2f456
Орать на Андрея!!!!!!!!!!!!!!!!!!!!!!!! <55321443+kot007@users.noreply.github.com>
2019-10-05 18:57:56 +0400

    Голова

--- MapObjects.cpp
+++ MapObjects.cpp
@@ -19,5 +19,21 @@ struct MapObject
     {
         Win32::TransparentBlt(txDC(), x,  y, x2 - x, y2 - y, picture, 0, 0, srk_width, srk_height, TX_WHITE);
     }
+
+      bool Click()
+    {
+        if (txMouseButtons() == 1 &&
+                txMouseX() > x  &&
+                txMouseX() < x2 &&
+                txMouseY() > y  &&
+                txMouseY() < y2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
