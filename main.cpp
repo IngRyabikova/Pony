155d273dc90913c57e4fad23acc0cb6e7862a289
Mike Milligram <55143644+KishkaShocoladki@users.noreply.github.com>
2019-10-10 16:59:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,18 +2,62 @@
 #include "Button.cpp"
 #include "MapObjects.cpp"
 
+void risyemKChasti (int COUNT_KAR, char* Kategorya, MapObject vybor_pony[], MapObject vybor_chasti[])
+{
+    for (int i = 0; i <=COUNT_KAR; i++)
+        {
+            if (vybor_pony [i].Click() and vybor_pony[i].Kategorya == Kategorya)
+            {
+                vybor_chasti[i].visible = !vybor_chasti[i].visible;
+                txSleep(200);
+            }
+        }
+}
+
+void risyemChasti (int COUNT_KAR, MapObject vybor_chasti[])
+{
+    for (int i = 0; i <=COUNT_KAR; i++)
+        {
+            if(vybor_chasti[i].visible)
+            {
+                vybor_chasti[i].drawMapObject2();
+            }
+        }
+}
+
+void risyemKPony (int COUNT_KAR, char* Kategorya, MapObject vybor_pony[])
+{
+    for (int i = 0; i <=COUNT_KAR; i++)
+        {
+            if (Kategorya == vybor_pony[i].Kategorya)
+            {
+                vybor_pony[i].drawMapObject2();
+            }
+        }
+}
+
+void risyemKnopki (int COUNT_BUTT, MenuButton buttons[])
+{
+        for (int i = 0; i <COUNT_BUTT; i++)
+        {
+            buttons[i].drawButton();
+        }
+}
+
 int main()
     {
     txCreateWindow(1200, 700);
 
-    MenuButton buttons[5];
+    const int COUNT_BUTT = 6;
+    MenuButton buttons[COUNT_BUTT];
     buttons[0] = {txLoadImage ("Pictures/Menu_Button.bmp"), 0,400,  0, 90,"Пони", 530, 140};
     buttons[1] = {txLoadImage ("Pictures/Menu_Button.bmp"), 0,400, 90,180,"Хвост", 530, 140};
     buttons[2] = {txLoadImage ("Pictures/Menu_Button.bmp"), 0,400,180,270,"Копыта", 530, 140};
     buttons[3] = {txLoadImage ("Pictures/Menu_Button.bmp"), 0,400,270,360,"Голова", 530, 140};
     buttons[4] = {txLoadImage ("Pictures/Menu_Button.bmp"), 0,400,360,450,"Тело", 530, 140};
 
-    MapObject vybor_pony[6];
+    const int COUNT_KAR = 6;
+    MapObject vybor_pony[COUNT_KAR];
     vybor_pony[0] = {1000,  0,1200,200,txLoadImage ("Pictures/Pony/pony1.bmp"), "Пони", 600, 400, true};
     vybor_pony[1] = {1000,200,1200,400,txLoadImage ("Pictures/Pony/pony2.bmp"), "Пони", 600, 400, true};
     vybor_pony[2] = {1000,400,1200,600,txLoadImage ("Pictures/Pony/pony4.bmp"), "Пони", 600, 400, true};
@@ -22,14 +66,13 @@ int main()
     vybor_pony[5] = {1000,400,1200,600,txLoadImage ("Pictures/Head/Head3.bmp"), "Хэд", 284, 219, true};
     char* Kategorya;
 
-    const int COUNT_KAR = 6;
     MapObject vybor_chasti[COUNT_KAR];
-    vybor_chasti[0] = {400, 100, 1000, 500,txLoadImage ("Pictures/Pony/pony1.bmp"), "Пони", 600, 400, false};
+    vybor_chasti[0] = {480, 100, 1080, 500,txLoadImage ("Pictures/Pony/pony1.bmp"), "Пони", 600, 400, false};
     vybor_chasti[1] = {400, 100, 1000, 500,txLoadImage ("Pictures/Pony/pony2.bmp"), "Пони", 600, 400, false};
     vybor_chasti[2] = {400, 100, 1000, 500,txLoadImage ("Pictures/Pony/pony4.bmp"), "Пони", 600, 400, false};
-    vybor_chasti[3] = {400, 100, 640, 270,txLoadImage ("Pictures/Head/Head1.bmp"), "Хэд", 259, 165, false};
-    vybor_chasti[4] = {400, 100, 640, 270,txLoadImage ("Pictures/Head/Head2.bmp"), "Хэд", 253, 178, false};
-    vybor_chasti[5] = {400, 100, 640, 270,txLoadImage ("Pictures/Head/Head3.bmp"), "Хэд", 284, 219, false};
+    vybor_chasti[3] = {500, 100, 740, 280,txLoadImage ("Pictures/Head/Head1.bmp"), "Хэд", 259, 165, false};//400, 100, 640, 270
+    vybor_chasti[4] = {500, 100, 740, 280,txLoadImage ("Pictures/Head/Head2.bmp"), "Хэд", 253, 178, false};
+    vybor_chasti[5] = {500, 100, 740, 280,txLoadImage ("Pictures/Head/Head3.bmp"), "Хэд", 284, 219, false};
     bool visible = false;
 
     while (!GetAsyncKeyState(VK_ESCAPE))
@@ -38,26 +81,11 @@ int main()
         txSetFillColor(TX_GREEN);
         txClear();
 
-        for (int i = 0; i < 5; i++)
-        {
-            buttons[i].drawButton();
-        }
+        risyemKnopki (5, buttons) ;
 
-        for (int i = 0; i <= 5; i++)
-        {
-            if (Kategorya == vybor_pony[i].Kategorya)
-            {
-                vybor_pony[i].drawMapObject2();
-            }
-        }
+        risyemKPony (6, Kategorya, vybor_pony);
 
-        for (int i = 0; i <=COUNT_KAR; i++)
-        {
-            if(vybor_chasti[i].visible)
-            {
-                vybor_chasti[i].drawMapObject2();
-            }
-        }
+        risyemChasti (6, vybor_chasti) ;
 
         if (buttons [3].Click())
         {
@@ -71,14 +99,7 @@ int main()
             txSleep(200);
         }
 
-        for (int i = 0; i <=COUNT_KAR; i++)
-        {
-            if (vybor_pony [i].Click() and vybor_pony[i].Kategorya == Kategorya)
-            {
-                vybor_chasti[i].visible = !vybor_chasti[i].visible;
-                txSleep(200);
-            }
-        }
+        risyemKChasti (6, Kategorya, vybor_pony, vybor_chasti);
 
         txSleep(10);
         txEnd();
